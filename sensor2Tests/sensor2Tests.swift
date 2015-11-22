db96e0183b2730e7ec7fedd09e7bbd142ca524d3
Greg McNutt <gcmcnutt@yahoo.com>
2015-11-21 19:52:46 -0800

    Initial Commit

---
+++ sensor2Tests/sensor2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  sensor2Tests.swift
+//  sensor2Tests
+//
+//  Created by Greg McNutt on 11/21/15.
+//  Copyright © 2015 Greg McNutt. All rights reserved.
+//
+
+import XCTest
+@testable import sensor2
+
+class sensor2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
