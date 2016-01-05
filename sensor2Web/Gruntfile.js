18dd846a5fe8d21db60f7a1cc0aaf84fcd654ac7
Greg McNutt <gcmcnutt@yahoo.com>
2016-01-04 21:23:44 -0800

    update the ui to render the dynamo data

---
+++ sensor2Web/Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+    
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+        example: {
+            port: 9000
+        }
+    },
+    uglify: {
+        options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        build: {
+            src: 'src/<%= pkg.name %>.js',
+            dest: 'build/<%= pkg.name %>.min.js'
+        }
+    },
+    concat: {
+        options: {
+            // define a string to put between each file in the concatenated output
+            separator: ';'
+        },
+        dist: {
+            // the files to concatenate
+            src: ['src/*.js'],
+            // the location of the resulting JS file
+            dest: 'dist/<%= pkg.name %>.js'
+        }
+    },
+    watch: {
+        scripts: {
+            files: ['**/*.js'],
+            tasks: ['concat'],
+            options: {
+                spawn: true,
+            },
+        },
+    }
+});
+    
+grunt.loadNpmTasks('grunt-connect');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('serve', ['connect:example']);
+grunt.registerTask('package', 'concat');
+
+    
+};
